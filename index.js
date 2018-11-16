643a0d3d5a038fd65319d5c0d0633a040f0bbb07
adnan1naeem <adnan.naeem@blueeast.com>
2018-11-16 14:13:49 +0500

    fixed code of react-native-drawer

--- index.js
+++ index.js
@@ -208,7 +208,7 @@ export default class Drawer extends Component {
 
     switch (this.props.type) {
       case 'overlay':
-        drawerProps[this.props.side] = Math.round(-this.getDeviceLength() + this._offsetOpen + this._length)
+        Math.round(-this.getDeviceLength() + this._offsetOpen + this._length)
         mainProps[this.props.side] = this._offsetClosed
         break
       case 'static':
